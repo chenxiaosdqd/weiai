6b22035b8d88332d6626dfdd7ddb83a3dd865d54
陈晓 <chenxiao@chenxiaodemacbook-air.local>
2016-04-17 21:26:44 +0800

    111111

---
+++ 11111/11111/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  11111
+//
+//  Created by 陈晓 on 16/4/17.
+//  Copyright © 2016年 陈晓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 11111/11111/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  11111
+//
+//  Created by 陈晓 on 16/4/17.
+//  Copyright © 2016年 陈晓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
